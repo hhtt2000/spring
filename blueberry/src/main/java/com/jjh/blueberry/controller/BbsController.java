c018d5b5904b83cd442be76c87cf8661030794da
hhtt2000 <hhtt2000@hanmail.net>
2015-07-16 22:18:56 +0900

    fix

--- blueberry/src/main/java/com/jjh/blueberry/controller/BbsController.java
+++ blueberry/src/main/java/com/jjh/blueberry/controller/BbsController.java
@@ -2,10 +2,12 @@ package com.jjh.blueberry.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BbsController {
 
+	@RequestMapping("/list.jh")
 	public String list(Model model){
 		
 		return "list";
